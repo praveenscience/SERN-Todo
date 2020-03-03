18032c4ff665c6c73ab60af7b8f71e863f810165
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2020-03-03 22:59:11 +0000

    Added body parser and implemented CRUD.

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const runSqlGetJson = require("../todos/db");
+const { runSqlGetJson, runSqlExecJson } = require("../todos/db");
 const route = express.Router();
+
+route.use(express.json());
+route.use(express.urlencoded({ extended: true }));
 
 route.get("/", (req, res) => {
   res.json("You're in the main API.");
@@ -8,6 +11,34 @@ route.get("/", (req, res) => {
 
 route.get("/todo", (req, res) => {
   runSqlGetJson("SELECT * FROM `todos`", undefined, res);
+});
+
+route.post("/todo", (req, res) => {
+  if (req.body && req.body.todo) {
+    runSqlExecJson("INSERT INTO `todos` (`content`, `done`) VALUES (?, ?)", [req.body.todo, 0], res);
+  } else {
+    res.status(400).json({ error: "Enter todo!" });
+  }
+});
+
+route.get("/todo/:id", (req, res) => {
+  runSqlGetJson("SELECT * FROM `todos` WHERE `id`=?", [req.params.id], res);
+});
+
+route.put("/todo/:id", (req, res) => {
+  if (req.body && req.body.todo && typeof req.body.done !== "undefined") {
+    runSqlExecJson("UPDATE `todos` SET `content`=?, `done`=? WHERE `id`=?", [req.body.todo, req.body.done, req.params.id], res);
+  } else {
+    res.status(400).json({ error: "Enter todo!" });
+  }
+});
+
+route.delete("/todo/:id", (req, res) => {
+  if (req.params && req.params.id) {
+    runSqlGetJson("DELETE FROM `todos` WHERE `id`=?", [req.params.id], res);
+  } else {
+    res.status(400).json({ error: "Enter todo id!" });
+  }
 });
 
 module.exports = route;
