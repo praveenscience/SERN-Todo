113aff75c2f0fcf045838888434a1f4ab5a0565e
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2020-03-03 21:32:30 +0000

    Added morgan middleware.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+
+app.use(morgan("dev"));
 
 app.listen(3100, () => {
   console.log("API Server Started in port 3100!");
